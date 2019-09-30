2a07aa545226e9d792fe681b59b859abe774f1eb
Tim Rayburn <tim@timrayburn.net>
2019-09-30 09:52:25 -0500

    injecting scanner

--- src/main/java/org/improving/tag/SpringContext.java
+++ src/main/java/org/improving/tag/SpringContext.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Random;
+import java.util.Scanner;
 
 @Configuration
 @ComponentScan("org.improving.tag")
 public class SpringContext {
     @Bean
     public Random getRandom() { return new Random(); }
+
+    @Bean
+    public Scanner getScanner() { return new Scanner(System.in); }
 }
